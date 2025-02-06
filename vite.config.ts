9f44fed77005c58b00fb8efb3039d9f8661573e8
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-02-06 16:22:25 +0700

    chore(config): clean up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: '',
