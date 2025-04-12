50726baf97dfb6149a99baaf283a4ba21b0854b2
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-04-12 22:29:57 +0700

    chore(interface): update meta context interface

--- src/interfaces/metaContext.ts
+++ src/interfaces/metaContext.ts
@@ -1,7 +1,5 @@
 interface IMetaContext {
   updateTitle: (newTitle: string) => void;
-  updateDescription: (newDescription: string) => void;
-  updateImage: (newImage: string) => void;
 }
 
 export default IMetaContext;
